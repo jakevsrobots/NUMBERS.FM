9129536bd2c05fb37d742345e0c53b4d40d65e34
jake <jake@dai5ychain.net>
2011-10-29 19:22:54 -0400

    bugfixed and refactored shuffling archive player

--- tools/ices_shuffle_playlist.py
+++ tools/ices_shuffle_playlist.py
@@ -6,44 +6,52 @@ import os, random
 
 
 # ----------------------
-# Constants
+# Playlist manager class
 # ----------------------
-ARCHIVE_BASE_DIR = '/opt/mp3/'
-NUM_RECENT_TRACKS_TO_COUNT = 10
-RECENTLY_PLAYED_TRACKS = []
-
-
-# ----------------------
-# Utilities
-# ----------------------
-def record_played_track(track):
-    RECENTLY_PLAYED_TRACKS.insert(track)
-    RECENTLY_PLAYED_TRACKS = RECENTLY_PLAYED_TRACKS[NUM_RECENT_TRACKS_TO_COUNT:]
+class PlaylistManager(object):
+    @classmethod
+    def get_manager(cls, *args, **kwargs):
+        if not hasattr(cls, '_manager'):
+            cls._manager = cls(*args, **kwargs)
+            
+        return cls._manager
     
-def get_random_track():
-    music_files = [
-        os.path.join(ARCHIVE_BASE_DIR, x) \
-            for x in os.listdir(ARCHIVE_BASE_DIR) \
-            if os.path.splitext(x)[1].lower() == 'mp3'
-    ]
+    def __init__(self, archive_base_dir='/opt/mp3/', num_tracks_to_count=10):
+        self.archive_base_dir = archive_base_dir
+        self.num_tracks_to_count = num_tracks_to_count
+        self.recently_played_tracks = []
 
-    counter = 0
-    limit = len(music_files)
-    while counter < limit:
-        track = random.choice(music_files)
-        if track not in RECENTLY_PLAYED_TRACKS:
-            record_played_track(track)
-            return track
+    def record_played_track(self, track):
+        self.recently_played_tracks.insert(0, track)
+        self.recently_played_tracks = self.recently_played_tracks[self.num_tracks_to_count:]
 
-    track = random.choice(music_files)
-    record_played_track(track)
-    return track
+    def get_random_track(self):
+        music_files = [
+            os.path.join(self.archive_base_dir, x) \
+                for x in os.listdir(self.archive_base_dir) \
+                if os.path.splitext(x)[1].lower() == '.mp3'
+        ]
+
+        track = None
+        counter = 0
+        limit = len(music_files)
+        while counter < limit:
+            counter += 1
+            track = random.choice(music_files)
+            if track not in self.recently_played_tracks:
+                break
+
+        if not track:
+            track = random.choice(music_files)
+
+        self.record_played_track(track)
+        return os.path.join(self.archive_base_dir, track)
 
 # ----------------------
 # Ices hooks
 # ----------------------
 def ices_init():
-    print "ices init"
+    print "ices_shuffle_playlist init"
     return 1    
 
 def ices_shutdown():
@@ -51,7 +59,7 @@ def ices_shutdown():
     return 1
 
 def ices_get_next():
-    return "/opt/mp3/numbersfm_intro.mp3"
+    return PlaylistManager.get_manager().get_random_track()
 
 def ices_get_metadata():
     return "You are listening to Numbers.FM"
