872a4c257b7d1d80a94206cb729f16763f3f55c6
jake <jake@dai5ychain.net>
2011-12-25 12:12:48 -0600

    finished update current song util

--- django/numbersfm/utils/icecast.py
+++ django/numbersfm/utils/icecast.py
@@ -1,11 +1,16 @@
 from urlparse import urljoin
+from urllib import urlencode
 import requests
 from django.conf import settings
 
 
 def update_current_song(song_name):
+    # ?mount=/listen&mode=updinfo&song=Archive+Stream
     url = urljoin(settings.ICECAST_SERVER, '/admin/metadata')
+    data = {'mount': settings.ICECAST_MOUNTPOINT,
+            'mode': 'updinfo',
+            'song': song_name}
+    url = '%s?%s' % (url, urlencode(data))
     r = requests.get(url, auth=(settings.ICECAST_ADMIN_USER,
                                 settings.ICECAST_ADMIN_PASSWORD))
-
-    # todo
+    
