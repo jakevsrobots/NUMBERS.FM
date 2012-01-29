92efe379a005428efc968f38327d6416c74a6433
jake <jake@dai5ychain.net>
2012-01-29 17:13:11 -0500

    clean-slated radio app migrations to deal with some weird foreignkey error it was throwing

--- django/numbersfm/radio/migrations/0001_initial.py
+++ django/numbersfm/radio/migrations/0001_initial.py
@@ -11,9 +11,9 @@ class Migration(SchemaMigration):
         # Adding model 'Show'
         db.create_table('radio_show', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=50, db_index=True)),
-            ('schedule_type', self.gf('django.db.models.fields.PositiveIntegerField')()),
             ('short_description', self.gf('django.db.models.fields.TextField')(blank=True)),
             ('full_description', self.gf('django.db.models.fields.TextField')(blank=True)),
             ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=255)),
@@ -22,25 +22,46 @@ class Migration(SchemaMigration):
         ))
         db.send_create_signal('radio', ['Show'])
 
+        # Adding model 'StationStatusUpdate'
+        db.create_table('radio_stationstatusupdate', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('is_live', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('current_show', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['radio.Show'], null=True, blank=True)),
+            ('current_song', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('timestamp', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+        ))
+        db.send_create_signal('radio', ['StationStatusUpdate'])
+
 
     def backwards(self, orm):
         
         # Deleting model 'Show'
         db.delete_table('radio_show')
 
+        # Deleting model 'StationStatusUpdate'
+        db.delete_table('radio_stationstatusupdate')
+
 
     models = {
         'radio.show': {
-            'Meta': {'object_name': 'Show'},
+            'Meta': {'ordering': "('name',)", 'object_name': 'Show'},
             'detail_page_image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
             'full_description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '255'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'schedule_type': ('django.db.models.fields.PositiveIntegerField', [], {}),
             'short_description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'}),
             'thumb_image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        'radio.stationstatusupdate': {
+            'Meta': {'ordering': "('-timestamp',)", 'object_name': 'StationStatusUpdate'},
+            'current_show': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['radio.Show']", 'null': 'True', 'blank': 'True'}),
+            'current_song': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_live': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'})
         }
     }
 
