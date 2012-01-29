bec03aec09212ada7a80325174a2e9cb5f34a1c8
jake <jake@dai5ychain.net>
2012-01-29 14:49:42 -0600

    station status form bugfixes

--- django/numbersfm/radio/controlpanel/forms.py
+++ django/numbersfm/radio/controlpanel/forms.py
@@ -3,7 +3,18 @@ from radio.models import Show, StationStatusUpdate
 
 
 class StationStatusUpdateForm(forms.ModelForm):
+    is_live = forms.ChoiceField(choices=(
+            (True, 'Live'),
+            (False, 'Archive')
+            ))
     
     class Meta:
         model = StationStatusUpdate
         exclude = ('timestamp',)
+
+    def clean(self):
+        # clear out extra data if it's an archive stream
+        if not self.cleaned_data.get('is_live') or self.cleaned_data.get('is_live') == 'False':
+            self.cleaned_data['current_show'] = None
+            self.cleaned_data['current_song'] = ''
+        return self.cleaned_data
--- django/numbersfm/radio/controlpanel/views.py
+++ django/numbersfm/radio/controlpanel/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from django.core.urlresolvers import reverse
 from radio.models import StationStatusUpdate
 from forms import StationStatusUpdateForm
 
 
+@user_passes_test(lambda u: u.is_staff, login_url='/secrets/login/')
 def dashboard(request):
     current_status = StationStatusUpdate.objects.current_status()
 
@@ -11,11 +14,11 @@ def dashboard(request):
         if form.is_valid():
             form.save()
             current_status = StationStatusUpdate.objects.current_status()
+            form = StationStatusUpdateForm(instance=current_status)
     else:
         form = StationStatusUpdateForm(instance=current_status)
     
     return render(request, 'radio/controlpanel/dashboard.html', {
             'current_status': current_status,
-            'form': StationStatusUpdateForm
+            'form': form
             })
-                  
